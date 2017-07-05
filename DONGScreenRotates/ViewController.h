b66263ce42663315b30ded5b457a6ae648b0dbfb
董国庆 <yesdgq@yahoo.com>
2017-07-05 15:18:12 +0800

    initialize project

---
+++ DONGScreenRotates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DONGScreenRotates
+//
+//  Created by yesdgq on 2017/7/5.
+//  Copyright © 2017年 yesdgq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
